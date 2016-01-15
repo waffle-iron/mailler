6629f530e0ea3f4e35b68985aa35f5d3c12a3851
alexandregama <alexandre.gama.lima@gmail.com>
2016-01-15 00:33:24 -0200

    Adds a Controller test to execute Spring MVC

---
+++ src/main/java/com/mailler/controller/MaillerApplication.java
@@ -0,0 +1,13 @@
+package com.mailler.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaillerApplication.class, args);
+	}
+	
+}
