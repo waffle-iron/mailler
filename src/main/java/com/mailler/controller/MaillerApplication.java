56ccb34faf5a0f2db19f203cee638fea1a79a6cc
alexandregama <alexandre.gama.lima@gmail.com>
2016-03-14 14:49:59 -0300

    Sending email to SES API using Email Template - WIP

--- src/main/java/com/mailler/controller/MaillerApplication.java
+++ src/main/java/com/mailler/controller/MaillerApplication.java
@@ -3,6 +3,15 @@ package com.mailler.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.context.config.annotation.EnableContextCredentials;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @SpringBootApplication
 @EnableContextCredentials
@@ -12,4 +21,43 @@ public class MaillerApplication {
 		SpringApplication.run(MaillerApplication.class, args);
 	}
 	
+	@Bean
+	public ViewResolver viewResolver() {
+	    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	    viewResolver.setTemplateEngine(templateEngine());
+	    return viewResolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+	    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+	    templateEngine.addTemplateResolver(emailTemplateResolver());
+	    templateEngine.addTemplateResolver(webTemplateResolver());
+	    templateEngine.addTemplateResolver(fileTemplateResolver());
+	    return templateEngine;
+	}
+	
+	private TemplateResolver emailTemplateResolver() {
+	    TemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+	    templateResolver.setPrefix("/mail/");
+	    templateResolver.setTemplateMode("HTML5");
+	    templateResolver.setOrder(1);
+	    return templateResolver;
+	}
+	
+	private TemplateResolver webTemplateResolver() {
+	    TemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+	    templateResolver.setPrefix("/WEB-INF/templates/");
+	    templateResolver.setTemplateMode("HTML5");
+	    templateResolver.setOrder(2);
+	    return templateResolver;
+	}
+
+	private ITemplateResolver fileTemplateResolver() {
+		TemplateResolver templateResolver = new FileTemplateResolver();
+		templateResolver.setPrefix("mail/");
+		templateResolver.setTemplateMode("HTML5");
+		templateResolver.setOrder(3);
+		return templateResolver;
+	}
 }
--- src/main/java/com/mailler/controller/SqsEmailConsumer.java
+++ src/main/java/com/mailler/controller/SqsEmailConsumer.java
@@ -1,30 +1,61 @@
 package com.mailler.controller;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailMessage;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring4.SpringTemplateEngine;
 
 @Component
 public class SqsEmailConsumer {
 
-	private MailSender mailSender;
-
 	@Autowired
-	public SqsEmailConsumer(MailSender mailSender) {
+	private JavaMailSender mailSender;
+	
+	@Autowired
+	private SpringTemplateEngine templateEngine;
+	
+	public SqsEmailConsumer() {
+	}
+
+	public SqsEmailConsumer(JavaMailSender mailSender, SpringTemplateEngine templateEngine) {
 		this.mailSender = mailSender;
+		this.templateEngine = templateEngine;
 	}
 	
 	@MessageMapping("https://sqs.us-east-1.amazonaws.com/669856304367/dev-email")
-	public void receive(Email email) {
+	public void receive(Email email) throws MessagingException {
 		System.out.println("Email received: " + email);
+
 		
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setTo(email.getEmailTo());
-		message.setFrom(email.getEmailFrom());
+		final Context ctx = new Context(Locale.getDefault());
+		ctx.setVariable("name", "RecipientName");
+		ctx.setVariable("subscriptionDate", new Date());
+		ctx.setVariable("hobbies", Arrays.asList("Cinema", "Sports", "Music"));
+		ctx.setVariable("imageResourceName", ""); // so that we can reference it from HTML
+		 
+		final String htmlContent = this.templateEngine.process("email-inlineimage.html", ctx);
+		
+		MimeMessage message = mailSender.createMimeMessage();
 		message.setSubject(email.getSubject());
-		message.setText(email.getContent());
+		
+		MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+		messageHelper.setTo(email.getEmailTo());
+		messageHelper.setFrom(email.getEmailFrom());
+		messageHelper.setText(htmlContent, true);
 		
 		System.out.println("Sending message: " + message);
 		this.mailSender.send(message);
